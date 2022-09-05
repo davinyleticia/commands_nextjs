a5a5a120f8be081abd0efd66db9cbb0b126db79f
Daviny Vidal <daviny@vidal.dev.br>
2022-09-04 23:20:04 -0300

    add web site

--- components/curso/Curso.js
+++ components/curso/Curso.js
@@ -4,12 +4,12 @@ import { Content, Description, Title } from './curso.styled';
 
 const Curso = () => {
   const services = [
-    { title : "Bash Básico", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
-    { title : "FileSytem LInux", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
-    { title : "Primeira Lei de Ohm", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
-    { title : "Arquitetura Básica do Computador", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
-    { title : "Reactjs Básico", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
-    { title : "Custom Hooks Reactjs", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
+    { title : "Bash Básico", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s", url: 'https://google.com' },
+    { title : "FileSytem LInux", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s", url: 'https://google.com'},
+    { title : "Primeira Lei de Ohm", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s", url: 'https://google.com'},
+    { title : "Arquitetura Básica do Computador", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s", url: 'https://google.com'},
+    { title : "Reactjs Básico", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s", url: 'https://google.com'},
+    { title : "Custom Hooks Reactjs", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s", url: 'https://google.com'},
   ]
   
   return (
@@ -27,7 +27,7 @@ const Curso = () => {
           {
             services.map((service, key) =>
               <Col key={key} lg={4} md={6}>
-                <Content>
+                <Content href={service.url}>
                   <Title className="text-dark font-weight-normal pt-1 mb-4">{service.title}</Title>
                   <Description className="text-muted mb-4">{service.desc}</Description>
                 </Content>
--- components/curso/curso.styled.js
+++ components/curso/curso.styled.js
@@ -1,13 +1,19 @@
 import styled from "styled-components";
 
 
-export const Content = styled.div`
+export const Content = styled.a`
     display: flex;
     flex-direction: column;
     margin: 1rem 0;
     padding: .5rem;
+    text-decoration: none;
+    border: none;
     border-radius: 5px;
     background-image: linear-gradient(420deg, #e9ecfe 10%, #e9ecbe 50%, #e9e1a1 10%);
+    &:hover {
+        text-decoration: none;
+        background-image: linear-gradient(420deg, #e5e1fe 20%, #e3e1be 30%, #e1d5a1 60%);
+    }
 `;
 
 export const Title = styled.h4`
