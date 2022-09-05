ef8f226d588a03b9414b5a148e167a713c888de7
Daviny Vidal <daviny@vidal.dev.br>
2022-09-04 21:48:50 -0300

    add web site

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Layout from "../components/Layout";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
-import Feature from '../components/Feature';
 import Service from '../components/Service';
 import About from '../components/About';
 import Footer from '../components/Footer';
