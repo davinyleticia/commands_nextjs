e90bc75b9c9eadae3cd9d16a2c9407191d3c0c48
Daviny Vidal <daviny@vidal.dev.br>
2023-04-05 01:17:13 -0300

    add header

--- views/tipsbook/header/Header.js
+++ views/tipsbook/header/Header.js
@@ -10,38 +10,19 @@ import {
 } from "reactstrap";
 import { NavbarHeader, Image } from "./header.styled";
 
-const Header = ({url}) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [sticky, setSticky] = useState(false);
-  const toggle = () => setIsOpen(!isOpen);
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-  });
-
-  const handleScroll = () => {
-    if (window.scrollY > 90) {
-      setSticky(true);
-    } else if (window.scrollY < 90) {
-      setSticky(false);
-    }
-  };
-
+const Header = ({ url }) => {
   return (
     <div className={`header${sticky ? "sticky" : ""}`}>
       <Navbar light expand="md">
         <Container>
-          <NavbarToggler onClick={toggle} />
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="m-auto" navbar>
-              <NavItem>
-                <NavLink href="/home">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/about">Sobre</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
+          <Nav className="m-auto" navbar>
+            <NavItem>
+              <NavLink href="/home">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/about">Sobre</NavLink>
+            </NavItem>
+          </Nav>
         </Container>
       </Navbar>
     </div>
