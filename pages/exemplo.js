2dc68ff45198b22d1daf82d91cb6b9bba10e2ecd
Daviny Vidal <daviny@vidal.dev.br>
2023-04-04 21:27:37 -0300

    addd

--- pages/exemplo.js
+++ pages/exemplo.js
@@ -1,9 +1,8 @@
-import Layout from "../components/Layout";
-import Header from "../components/header/Header";
-import Hero from "../components/Hero";
-import Curso from '../components/curso/Curso';
-import About from '../components/About';
-import Footer from '../components/Footer';
+import Layout from "../views/afulink/Layout";
+import Header from "../views/afulink/header/Header";
+import Curso from "../views/afulink/Hero";
+import Footer from "../views/afulink/Footer";
+import Hero from "../views/afulink/Hero";
 
 const Index = () => {
   return (
