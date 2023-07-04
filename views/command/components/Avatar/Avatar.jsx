e440da3a144f0c84ca1d1383e4d289360f22dbf0
Daviny Vidal <daviny@vidal.dev.br>
2023-07-03 22:56:06 -0300

    add

--- views/command/components/Avatar/Avatar.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Container } from './avatar.styled';
-
-function Avatar({ imgUrl, label }) {
-  return <Container src={imgUrl} atl={label} />;
-}
-
-export default Avatar;
