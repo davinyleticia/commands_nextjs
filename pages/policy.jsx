b168478136202499b34ab6c2efdc1b3965791fbb
Daviny Vidal <daviny@vidal.dev.br>
2023-07-13 18:37:44 -0300

    new policy

--- pages/policy.jsx
+++ pages/policy.jsx
@@ -1,12 +1,13 @@
 import Layout from "../views/afulink/Layout";
 import Header from "../views/afulink/header/Header";
 import Footer from "../views/afulink/Footer";
+import PolicyPage from "../views/afulink/policy";
 
 const Policy = () => {
   return (
     <Layout pageTitle="Afulink Informática">
       <Header url={'..'} />
-      <Policy />
+      <PolicyPage />
       <Footer />
     </Layout>
   )
