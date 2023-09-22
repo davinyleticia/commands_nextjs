56294f5e2725bd97234efdf7efea8415cdddabf3
Daviny Vidal <daviny@vidal.dev.br>
2023-09-22 19:21:32 -0300

    asss

--- views/afulink/blog/index.jsx
+++ views/afulink/blog/index.jsx
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "reactstrap";
-import { Title } from "../../command/components/NotificationsCard/Notifications.styled";
+import TBIco from "../ico/tb";
+import {
+  Banner,
+  Card,
+  Li,
+  Text,
+  TitleProject,
+  Ul,
+} from "./styled";
 
 const Blog = ({ itemsApi }) => {
-  console.log(itemsApi);
-  return (
+  console.log(itemsApi?.map((elem) => elem));
+  const [isClient, setIsClient] = useState(false)
+ 
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+
+  return (<>{isClient ? (
     <Container>
+      <Banner>
+        <TBIco />
+      </Banner>
       <React.Fragment>
-        {itemsApi.map((elem) => {
-          <Title>{elem.title.rendered}</Title>;
-        })}
+        <Ul>
+          {itemsApi?.map((item) => (
+            <Li key={item.id}>
+              <img src={item.image} alt={item.name} />
+              <Card>
+                <TitleProject>{item.title.rendered}</TitleProject>
+                <Text
+                  dangerouslySetInnerHTML={{
+                    __html: item.excerpt.rendered || "Sem descrição",
+                  }}
+                />
+              </Card>
+            </Li>
+          ))}
+        </Ul>
       </React.Fragment>
     </Container>
-  );
+  ): 'Prerendered'}</>);
 };
 
 export default Blog;
---
+++ views/afulink/blog/styled.jsx
@@ -0,0 +1,169 @@
+import styled from "styled-components";
+
+export const Banner = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 10rem;
+  background: #1b6a94;
+  padding: 1rem;
+  margin: 1rem 0;
+`;
+
+export const Title = styled.h4`
+  padding: 0 !important;
+  margin: 0 !important;
+`;
+export const Description = styled.strong`
+  display: flex;
+  flex-direction: row;
+  margin-top: 1rem;
+  color: #f1e8e8;
+  size: 20px;
+  padding: 1rem;
+  max-width: 26rem;
+`;
+
+export const Ul = styled.ul`
+  display: grid;
+  grid-template-columns: 36% 36% 36%;
+  -webkit-box-pack: center;
+  justify-content: center;
+  margin: 2rem 0px;
+  width: 95%;
+
+  &::-webkit-scrollbar {
+    border-radius: 10px;
+    background-color: #f1e8e8;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background-color: #f1e8e8;
+  }
+
+  @media (max-width: 1265px) {
+    display: flex;
+    flex-direction: column;
+    width: 90%;
+  }
+`;
+
+export const CardProject = styled.li`
+  display: flex;
+  flex-direction: column;
+  margin: 10px 10px 20px;
+  justify-content: flex-start;
+  /* box-shadow: 1px 1px 1px 2px gray; */
+  border: 1px solid gray;
+  gap: 5px;
+  border-radius: 10px;
+  padding: 1rem;
+  &:hover {
+    background: #f0efef;
+    opacity: 0.8;
+  }
+  img {
+    border-top-left-radius: inherit;
+    border-top-right-radius: inherit;
+    width: 100%;
+    height: 14rem;
+  }
+
+  @media (max-width: 1265px) {
+    font-size: 13px;
+    min-height: 8rem;
+  }
+`;
+
+export const Li = styled.li`
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  word-break: break-word;
+  margin: 10px 10px 20px;
+  justify-content: space-between;
+  /* box-shadow: 1px 1px 1px 2px gray; */
+  border: 1px solid #f1e8e8;
+  gap: 5px;
+  border-radius: 10px;
+  cursor: pointer;
+
+  &:hover {
+    background: #f0efef;
+
+    opacity: 0.8;
+  }
+  img {
+    border-top-left-radius: inherit;
+    border-top-right-radius: inherit;
+    width: 100%;
+    height: 14rem;
+  }
+
+  @media (max-width: 1265px) {
+    font-size: 13px;
+    min-height: 8rem;
+  }
+`;
+
+export const TitleProject = styled.strong``;
+
+export const Url = styled.button`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background: transparent;
+  color: #fff;
+  border: none;
+  text-decoration: none;
+  gap: 1rem;
+`;
+
+export const Text = styled.p`
+  text-align: start;
+  margin: 0.5rem 0;
+`;
+
+export const Dot = styled.div`
+  position: relative;
+  top: 1px;
+  display: inline-block;
+  width: 12px;
+  height: 12px;
+  border: 1px solid #563d7c;
+  border-radius: 50%;
+  background: #563d7c;
+  margin-right: 1rem;
+`;
+
+export const DescriptionBtn = styled(Description)`
+  cursor: pointer;
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+  padding: 1rem 0;
+  p {
+    padding: 0 1rem;
+  }
+  @media (max-width: 1265px) {
+    justify-content: center;
+  }
+`;
+
+export const GroupIcons = styled.div`
+  bottom: 0;
+  display: flex;
+  justify-content: space-between;
+  z-index: 1000;
+  margin: 1rem;
+`;
+
+export const Card = styled.div`
+  padding: 1rem 1.7rem  1.7rem  1.7rem ;
+  display: flex
+  flex-direction:column;
+  justify-content: space-between;
+`;
