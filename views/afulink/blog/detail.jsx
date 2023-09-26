b45a23b4dddc588eab10503c006bdcadf90cc81e
Daviny Vidal <daviny@vidal.dev.br>
2023-09-26 18:36:21 -0300

    adds

--- views/afulink/blog/detail.jsx
+++ views/afulink/blog/detail.jsx
@@ -11,29 +11,49 @@ const Detail = ({ itemsApi }) => {
   }, []);
 
   return (
-    <><hr></hr>
+    <>
+      <hr></hr>
       {isClient ? (
         <Container>
           <Banner>
-          <a href="../tipsbook">
+            <a href="../tipsbook">
               <TBIco />
               <p>TipsBook</p>
             </a>
           </Banner>
           <hr></hr>
           <React.Fragment>
-            <div>
-              {itemsApi?.map((item) => (
-                <div key={item.id}>
-                  <Title>{item.title.rendered}</Title>
-                  <Text
-                    dangerouslySetInnerHTML={{
-                      __html: item.content.rendered || "Sem descrição",
-                    }}
-                  />
-                </div>
-              ))}
-            </div>
+            <>
+              <div>
+                {itemsApi?.map((item) => (
+                  <div key={item.id}>
+                    <Title>{item.title.rendered}</Title>
+                    <Text
+                      dangerouslySetInnerHTML={{
+                        __html: item.content.rendered || "Sem descrição",
+                      }}
+                    />
+                  </div>
+                ))}
+              </div>
+              <script
+                async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3182119963461273"
+                crossorigin="anonymous"
+              ></script>
+
+              <ins
+                class="adsbygoogle"
+                style="display:block"
+                data-ad-client="ca-pub-3182119963461273"
+                data-ad-slot="4888533084"
+                data-ad-format="auto"
+                data-full-width-responsive="true"
+              ></ins>
+              <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+              </script>
+            </>
           </React.Fragment>
         </Container>
       ) : (
