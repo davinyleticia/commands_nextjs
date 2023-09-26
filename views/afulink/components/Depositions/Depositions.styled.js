367529f14ab7f03609600520d9b9364e72d0ef2b
Daviny Vidal <daviny@vidal.dev.br>
2023-09-25 22:14:21 -0300

    add Depositions

---
+++ views/afulink/components/Depositions/Depositions.styled.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Titulo = styled.h2`
+  padding-top: 1rem;
+  text-align: center;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 3rem 0;
+  align-items: center;
+  @media (max-width: 600px) {
+    flex-direction: column;
+  }
+`;
+
+export const Card = styled.div`
+
+border: 5px solid #1011;
+padding: 1rem;
+width: 100%;
+border-radius: 10px;
+img {
+    margin: 0 1rem;
+  };
+  spam{
+    display: flow;
+    text-align: center;
+  }
+`;
